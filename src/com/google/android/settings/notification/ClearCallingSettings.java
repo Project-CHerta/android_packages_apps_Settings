c2eb4c3ed0c733d9dfbdd33ddefeb5bf0d98f7a4
Anay Wadhera <anay1018@gmail.com>
2023-05-28 17:08:05 -0400

    Settings: Import Clear Calling
    Change-Id: I1bbdd39dbb7bd772efb564d355b696c1657254cd
    
    [ralph950412] Update to bp3a

---
+++ src/com/google/android/settings/notification/ClearCallingSettings.java
@@ -0,0 +1,28 @@
+package com.google.android.settings.notification;
+
+import android.app.settings.SettingsEnums;
+
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+
+import com.android.settings.R;
+
+public class ClearCallingSettings extends DashboardFragment {
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.clear_calling_settings);
+
+    @Override
+    protected String getLogTag() {
+        return "ClearCallingSettings";
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.CLEAR_CALLING;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.clear_calling_settings;
+    }
+}
